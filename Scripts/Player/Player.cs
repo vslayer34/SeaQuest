8c96737a4c3887bf97491e9cd69c8972379db328
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2024-06-23 11:33:53 +0300

    Trying to adjust the shooting direction when rotating the submarine

--- Scripts/Player/Player.cs
+++ Scripts/Player/Player.cs
@@ -9,10 +9,16 @@ public partial class Player : AnimatedSprite2D
     [Export]
     private float _speed = 50.0f;
 
+    [Export]
+    public BulletShooter BulletShooterComponent { get; private set; }
+
 
     private Vector2 _velocity;
 
     private const double IDLE_ANIMATION_SPEED = 5.0;
+
+    private readonly Vector2 _faceingRightBulletSpawnerPosition = new Vector2(13, 2);
+    private readonly Vector2 _faceingLeftBulletSpawnerPosition = new Vector2(-13, 2);
 
 
 
@@ -46,11 +52,27 @@ public partial class Player : AnimatedSprite2D
             else
             {
                 FlipH = _velocity.X < 0;
+                RotateShootingPoints(_velocity.X > 0);
             }
         }
         else
         {
             SpriteFrames.SetAnimationSpeed(AnimationsNames.Player.DEFAULT, IDLE_ANIMATION_SPEED);
+        }
+    }
+
+
+    private void RotateShootingPoints(bool facingRight)
+    {
+        if (facingRight)
+        {
+            BulletShooterComponent.Position = _faceingRightBulletSpawnerPosition;
+            BulletShooterComponent.RotationDegrees = 0.0f;
+        }
+        else
+        {
+            BulletShooterComponent.Position = _faceingLeftBulletSpawnerPosition;
+            BulletShooterComponent.RotationDegrees = 180.0f;
         }
     }
 }
